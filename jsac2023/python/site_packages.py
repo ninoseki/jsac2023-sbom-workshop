8d8c7ce5c4366f06bfcfba8bc67dbf44520bb2ed
Manabu Niseki <manabu.niseki@gmail.com>
2022-11-20 11:41:36 +0900

    chore: remove answers [skip ci]

--- jsac2023/python/site_packages.py
+++ jsac2023/python/site_packages.py
@@ -1,55 +1,12 @@
-import json
-from typing import cast
-
 import pkg_resources
 import psutil
 from cyclonedx.model.component import Component, Property
 from packageurl import PackageURL
 
-from jsac2023.utils import get_command
-
-from .process import am_i_root, get_python_in_cmdline
-
 
 def get_site_packages(process: psutil.Process) -> set[str]:
     """Get site packages used by a Python process"""
-    site_packages: set[str] = set()
-    output = None
-
-    # Python may be nested like the following:
-    # - exe: /usr/local/bin/python3.10
-    # - cmdline: [/home/vscode/.cache/pypoetry/virtualenvs/jsac2023-oBdw23Ej-py3.10/bin/python]
-    # (when using virtualenv)
-    try:
-        exe = get_python_in_cmdline(process.cmdline()) or process.exe()
-        user = process.username()
-    except psutil.Error:
-        return site_packages
-
-    get_site_packages_command = (
-        "import site;import json;print(json.dumps(site.getsitepackages()))"
-    )
-    if am_i_root():
-        sudo = get_command("sudo")
-        try:
-            c = f'{exe} -c "{get_site_packages_command}"'
-            output = sudo("su", user, "-c", c)
-        except Exception:
-            return site_packages
-    else:
-        py = get_command(exe)
-        try:
-            output = py("-c", get_site_packages_command)
-        except Exception:
-            return site_packages
-
-    if output is None:
-        return site_packages
-
-    site_packages_ = cast(list[str], json.loads(str(output)))
-    site_packages.update(site_packages_)
-
-    return site_packages
+    raise NotImplementedError()
 
 
 def distribution_to_component(dist: pkg_resources.Distribution) -> Component:
