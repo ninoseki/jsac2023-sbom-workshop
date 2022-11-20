a35348c29ebf04232bae14733900e895fbe3e9a2
Manabu Niseki <manabu.niseki@gmail.com>
2022-11-20 10:42:56 +0900

    chore: rename the repository

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+echo "Setting up the lab env..."
+
 # run Python & Java processes
 cd /app
 nohup uvicorn python-vulnerable-app.main:app &>/dev/null &
 nohup java -jar /app/log4j-vulnerable-app/spring-boot-application.jar &>/dev/null &
 
 # setup Poetry
-cd /workspaces/jsac2023
+cd /workspaces/jsac2023-sbom-workshop
 pip install --user -r requirements.txt --quiet
 
-rm -rf .venv
-poetry config virtualenvs.path /home/vscode/.cache/pypoetry/virtualenvs/jsac2023-py3.10/
+poetry config virtualenvs.path /home/vscode/.cache/pypoetry/virtualenvs/jsac2023-sbom-workshop-py3.10/
 poetry install --no-ansi -q -n
+
+echo "Done! You are ready for the workshop."
