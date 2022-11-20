fdf76a2a0b46e9c74954fc54b44e84f517dc7d61
Manabu Niseki <manabu.niseki@gmail.com>
2022-11-20 19:49:44 +0900

    refactor: change to use in-project venv

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -9,9 +9,13 @@ nohup java -jar /app/log4j-vulnerable-app/spring-boot-application.jar &>/dev/nul
 
 # setup Poetry
 cd /workspaces/jsac2023-sbom-workshop
+
+echo "Install Poetry..."
 pip install --user -r requirements.txt --quiet
 
-poetry config virtualenvs.path /home/vscode/.cache/pypoetry/virtualenvs/jsac2023-sbom-workshop-py3.10/
+poetry config virtualenvs.in-project true --local
+
+echo "Install depdencies by Poetry..."
 poetry install --no-ansi -q -n
 
 echo "Done! You are ready for the workshop."
