075ca1a15cf304cd88e6d62cc959f977755541e3
Manabu Niseki <manabu.niseki@gmail.com>
2022-11-23 11:47:24 +0900

    chore: remove the implementation

--- jsac2023/java/jar.py
+++ jsac2023/java/jar.py
@@ -1,70 +1,8 @@
-import tempfile
-import zipfile
 from pathlib import Path
 
 from cyclonedx.model.component import Component
-from packageurl import PackageURL
-
-
-def to_component(*, group: str, artifact: str, version: str) -> Component:
-    component_name = f"{group}:{artifact}"
-    return Component(
-        name=component_name,
-        version=version,
-        purl=PackageURL(type="maven", namespace=group, name=artifact, version=version),
-    )
-
-
-def parse_pom_properties(path: str | Path) -> Component | None:
-    memo: dict[str, str] = {}
-
-    with open(path) as f:
-        lines = f.readlines()
-        for line in lines:
-            if line.startswith("#"):
-                continue
-
-            try:
-                key, value = line.strip().split("=")
-                memo[key] = value
-            except Exception:
-                pass
-
-    group = memo.get("groupId")
-    artifact = memo.get("artifactId")
-    version = memo.get("version")
-
-    if group is None or artifact is None or version is None:
-        return None
-
-    return to_component(group=group, artifact=artifact, version=version)
-
-
-def unzip_jar(path: str | Path, *, destination: str):
-    with zipfile.ZipFile(path) as zip:
-        names = [Path(name) for name in zip.namelist()]
-
-        # Extract .jar or .properties files
-        jar_or_properties = [
-            str(name) for name in names if name.suffix in [".jar", ".properties"]
-        ]
-        zip.extractall(destination, members=jar_or_properties)
 
 
 def parse_jar(path: str | Path) -> list[Component]:
     """Parse JAR file and convert it into a list of components"""
-    components: list[Component] = []
-
-    with tempfile.TemporaryDirectory() as destination:
-        # unzip JAR file into a temporal directory
-        unzip_jar(path, destination=destination)
-
-        pom_properties = Path(destination).glob("**/pom.properties")
-        pom_components = [parse_pom_properties(p) for p in pom_properties]
-        components.extend([c for c in pom_components if c is not None])
-
-        jar_paths = Path(destination).glob("**/*.jar")
-        for jar_path in jar_paths:
-            components.extend(parse_jar(jar_path))
-
-    return components
+    raise NotImplementedError()
